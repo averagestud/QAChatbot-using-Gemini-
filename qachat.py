217ef14b87f0f6b323a9aeb294138eeb788a3362
Deepaprakash K <128608033+averagestud@users.noreply.github.com>
2024-01-01 15:32:07 +0530

    Add files via upload

---
+++ qachat.py
@@ -0,0 +1,47 @@
+from dotenv import load_dotenv
+load_dotenv()
+GOOGLE_API_KEY="AIzaSyC_viZYPOrne13GxqCanPxbQnqaLCh1md0"
+# In this chatbot I am using Gemini Pro API to create a simple conversational chatbot
+import streamlit as st
+import os 
+import google.generativeai as genai
+genai.configure(api_key=os.getenv(GOOGLE_API_KEY))
+
+# function to load gemini
+model=genai.GenerativeModel("gemini-pro")
+chat = model.start_chat(history=[])
+
+def get_gemini_response(question):
+    response= chat.send_message(question, stream=True)
+    return response
+
+# Initialising the streamlit app to run the model
+st.set_page_config(page_title="Q&A Chatbot")
+
+st.header("Chatbot using Gemini Pro")
+
+# Initialize the session to store history of the chat performed with the chatbot
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history']=[]
+
+# Take the input for the query from the user
+input=st.text_input("Input: ", key="input")
+submit=st.button("Ask the question to be answered")
+
+# As soon as the user submits the input we will generate a response for the user
+if submit and input:
+    response = get_gemini_response(input)
+
+    # Add user query and the bot response to the chat history
+    st.session_state['chat_history'].append(("You", input))
+    st.subheader("The response is:")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot", chunk.text))
+
+st.subheader("The chat history is:")
+
+for role,text in st.session_state['chat_history']:
+    st.write(f"{role} : {text}")
+
+ 
